5fa66a72a1d83def7d14e632db558211e261ac03
Adam C Hamlin <achamlin@gmail.com>
2022-12-17 10:27:23 -0500

    fix: Properly package for publishing

--- index.ts
+++ index.ts
@@ -1,5 +1,2 @@
-import { DeepMap } from './src/map';
-import { DeepSet } from './src/set';
-
-export { DeepMap };
-export { DeepSet };
+export { DeepMap } from './src/map';
+export { DeepSet } from './src/set';
