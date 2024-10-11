5493ee8ab2e9ae10b305bba5042c7d1eedc7ce70
adamhamlin <18332856+adamhamlin@users.noreply.github.com>
2024-10-11 17:22:37 -0400

    chore: export `Options` type (#21)

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export { DeepSet } from './src/set';
 export { DeepMap } from './src/map';
 export { BiDirectionalDeepMap } from './src/map.bi-directional';
+export { Options } from './src/options';
 export { areEqual } from './src/areEqual';
